0998e716e1a597abb88d4105f532b5a27c75543f
Brett D Burdick <burdickbrett@outlook.com>
2016-09-09 16:18:36 -0400

    MEAN

---
+++ back-end/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var mongoUrl = "mongodb://localhost:27017/students";
+var Student = require('../models/students');
+mongoose.connect(mongoUrl);
+
+// var student = new Student({
+// 	name: "JT",
+// 	gender: "male"
+// });
+
+// student.save();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/getStudents', function(req, res, next){
+	Student.find({}, function(error, documents){
+		res.json(documents);
+	});
+	
+});
+module.exports = router;
